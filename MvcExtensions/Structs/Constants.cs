5dd7a5a62742d5cd1f400ceab9de89c61fa4d8f2
Simeone Vilardo <simeone.vilardo@gmail.com>
2016-09-11 21:19:21 +0200

    Fix DIALOG_MESSAGE.

--- MvcExtensions/Structs/Constants.cs
+++ MvcExtensions/Structs/Constants.cs
@@ -6,7 +6,7 @@
         {
             public const string DIALOG_CLOSE_BUTTON_TEXT = "Annulla";
             public const string DIALOG_OK_BUTTON_TEXT = "Ok";
-            public const string DIALOG_MESSAGE = @"Sei sicuro di voler eseguire l\'operazione?";
+            public const string DIALOG_MESSAGE = "Sei sicuro di voler eseguire l\'operazione?";
             public const string DIALOG_TITLE = "Attenzione";
             public const string DIALOG_OPEN_BUTTON_TEXT = "Esegui";
         }
